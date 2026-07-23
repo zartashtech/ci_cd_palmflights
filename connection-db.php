247f7fcc141763268013c20dd205ded4050a074c
zartashtech <host@zartash.com>
2026-07-23 20:40:43 +0500

    Change database connection details
    Updated database connection credentials for production.

--- connection-db.php
+++ connection-db.php
@@ -1,7 +1,7 @@
 <?php
- $server = 'localhost';
- $user = 'fms_web';
- $pass = 'r1@ie*.hre32';
+ $server = 'fms.candfsystems.com';
+ $user = 'fms_web_cfdev';
+ $pass = 'Br2*a7b+!roVAR"';
  $db = 'fms_db';
 $con=mysqli_connect("$server","$user","$pass","$db");
 if (mysqli_connect_errno()) {
