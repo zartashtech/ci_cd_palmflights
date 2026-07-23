fa38141349697a85c16356cf267ffef1e96f9a0c
zartashtech <host@zartash.com>
2026-07-23 21:05:08 +0500

    Update global.php

--- global.php
+++ global.php
@@ -2,7 +2,7 @@
 // Global DB configuration — shared across the app.
  $server = 'fms.candfsystems.com';
  $user = 'fms_web_cfdev';
- $pass = 'Br2*a7b+!roVAR"';
+ $pass = 'Br2*a7b+!roVAR';
  $db = 'fms_db';
 ?>
 
